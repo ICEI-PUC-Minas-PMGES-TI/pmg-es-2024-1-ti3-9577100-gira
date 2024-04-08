157264e2e558d2582caf5d8384891dfd5c59e09c
Gabriel Henrique Miranda Rodrigues <128641766+GabrielHMR2002@users.noreply.github.com>
2024-04-08 17:22:54 -0300

    Update NewsController.java

--- Codigo/back-end/pucTis/pucTis/src/main/java/com/app/pucTis/Controllers/NewsController.java
+++ Codigo/back-end/pucTis/pucTis/src/main/java/com/app/pucTis/Controllers/NewsController.java
@@ -1,6 +1,7 @@
 package com.app.pucTis.Controllers;
 import com.app.pucTis.Dtos.NewsRecord;
 import com.app.pucTis.Entities.News;
+import com.app.pucTis.Repositories.NewsRepository;
 import com.app.pucTis.Services.NewsService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -16,6 +19,8 @@ import java.util.Optional;
 public class NewsController {
     @Autowired
     private NewsService newsService;
+    @Autowired
+    NewsRepository newsRepository;
 
 
     @PostMapping
@@ -30,6 +35,9 @@ public class NewsController {
         return ResponseEntity.ok(likesCount);
     }
 
-
+    @GetMapping("/getNews")
+    public ResponseEntity<List<News>> getAllNews(){
+        return ResponseEntity.ok(newsRepository.findAll());
+    }
 
 }
